8f4a8119f482638d4db91a72089f078bfc334e79
notrlyrainy <ruinaliu2007@gmail.com>
2026-07-16 17:57:23 -0500

    types added for app, assignment, and review

---
+++ lib/types.ts
@@ -0,0 +1,42 @@
+export type Application = {
+    timestamp: string,
+    email: string,
+    name: string,
+    uscId: string,
+    phone: string,
+    pronouns: string,
+    year: string,
+    majorMinor: string,
+    coursework: string,
+    howHeard: string,
+    links: string,
+    whyJoin: string,
+    aiResponse: string,
+    experienceResponse: string,
+    socialResponse: string,
+    passionResponse: string,
+    availability: string,
+    maybeExplanation: string,
+    notifsReaction: string
+};
+
+export type AssignmentStatus = "assigned" | "in progress" | "recused" | "completed";
+
+export type Assignment = {
+  uscId: string,
+  reviewerEmail: string,
+  status: AssignmentStatus
+};
+
+export type ReviewScore = 1 | 2 | 3;
+
+export type Review = {
+    uscId: string,
+    reviewerName: string,
+    reviewerEmail: string,
+    experienceScore: ReviewScore,
+    researchScore: ReviewScore,
+    qualityScore: ReviewScore,
+    notes: string,
+    submittedAt: string
+};
